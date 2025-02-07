9986ca52a91a423b9a1c7372bfe74ee7fc323036
Jeo738 <jeffersonamorim738@gmail.com>
2025-02-07 13:03:01 -0300

    Loop

---
+++ src/Loop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args){
+
+        double mediaAvaliacao = 0;
+        System.out.println("Digite a quantidade de notas: ");
+        Scanner repet = new Scanner(System.in);
+       int i = repet.nextInt();
+        for ( i=0;i<3; i++){
+            Scanner entradaNota = new Scanner(System.in);
+            System.out.println("Digite a nota do filme: ");
+            double nota= entradaNota.nextDouble();
+            mediaAvaliacao += nota;
+        }
+
+        System.out.println("Média de avaliação: " + mediaAvaliacao/i);
+    }
+}
