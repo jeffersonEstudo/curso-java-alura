56f33fbfdb570b80181ade0008dafa6f10a3d3be
Jeo738 <jeffersonamorim738@gmail.com>
2025-02-13 14:34:06 -0300

    GameAdivinha

--- src/GameAdivinha.java
+++ src/GameAdivinha.java
@@ -7,20 +7,20 @@ public class GameAdivinha {
 
         Scanner leitura = new Scanner(System.in);
         System.out.println("GAME DE ADIVINHAÇÂO");
-        System.out.println("Digite um número");
+        System.out.println("Digite um número:");
+        System.out.println();
         int numero = leitura.nextInt();
         int tentativas = 5;
-        Random random = new Random();
-        int numeroAleatorio = random.nextInt();
+        int numeroAleatorio = new Random().nextInt(10);
 
         for (int i = 0; i <= tentativas ; i++){
 
-            new Random(numeroAleatorio).nextInt(100);
+
             if(numero != numeroAleatorio){
-                System.out.println("Você errou. O número correto é: " + numeroAleatorio);
+                System.out.println("Você errou. Número correto é: " + numeroAleatorio);
                 break;
             }else {
-                System.out.println("Você ACERTOU!" + numero);
+                System.out.println("Você ACERTOU! " + numero);
                 break;
             }
 
