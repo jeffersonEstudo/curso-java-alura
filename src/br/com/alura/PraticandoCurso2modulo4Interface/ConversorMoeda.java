1c9623fc10c4caf4643fb33dc08095e807ce56d6
jeffersonEstudo <jefferson11oliveira@gmail.com>
2025-02-26 14:51:05 -0300

    Interface

---
+++ src/br/com/alura/PraticandoCurso2modulo4Interface/ConversorMoeda.java
@@ -0,0 +1,21 @@
+package br.com.alura.PraticandoCurso2modulo4Interface;
+
+public class ConversorMoeda implements ConversorFinanceira{
+    protected double valor;
+
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public double converterDolarParaReal() {
+        valor = valor * 5.78;
+        System.out.println(valor);
+        return valor;
+    }
+}
