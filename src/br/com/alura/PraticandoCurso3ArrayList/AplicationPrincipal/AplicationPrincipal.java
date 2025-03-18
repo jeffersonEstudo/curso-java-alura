f9b897511659638d52ca7625bed76b6be94fe107
jeffersonEstudo <jefferson11oliveira@gmail.com>
2025-03-18 14:20:59 -0300

    Curso3ArrayListModulo1Praticando

--- src/br/com/alura/PraticandoCurso3ArrayList/AplicationPrincipal/AplicationPrincipal.java
+++ src/br/com/alura/PraticandoCurso3ArrayList/AplicationPrincipal/AplicationPrincipal.java
@@ -8,12 +8,23 @@ public class AplicationPrincipal {
     public static void main(String[] args) {
 
         Pessoas objPessoas = new Pessoas();
+        Pessoas objPessoas2 = new Pessoas();
+        Pessoas objPessoas3 = new Pessoas();
         objPessoas.setNome("Jefferson");
-        objPessoas.setIdade(33);
+        objPessoas2.setNome("Israel");
+        objPessoas3.setNome("Oto");
+        objPessoas.setIdade(27);
+        objPessoas2.setIdade(22);
+        objPessoas3.setIdade(30);
 
         ArrayList<Pessoas> listaPessoas = new ArrayList<>();
          listaPessoas.add(objPessoas);
-        System.out.println(listaPessoas.get(0).toString());
+         listaPessoas.add(objPessoas2);
+         listaPessoas.add(objPessoas3);
+        System.out.println("Tamanho da lista: " + listaPessoas.size());
+        System.out.println("Primeira pessoa: " + listaPessoas.get(0).getNome());
+        System.out.println(listaPessoas);
+
 
     }
 }
