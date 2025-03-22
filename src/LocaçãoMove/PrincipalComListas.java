9b96838c059f8e339853279681278a004599a35c
jeffersonEstudo <jefferson11oliveira@gmail.com>
2025-03-21 22:13:03 -0300

    Modulo3Curso3

---
+++ src/LocaçãoMove/PrincipalComListas.java
@@ -0,0 +1,27 @@
+package LocaçãoMove;
+
+import br.com.alura.screenmath.modelosLocacaoMove.Modelos.Filme;
+import br.com.alura.screenmath.modelosLocacaoMove.Modelos.Serie;
+import br.com.alura.screenmath.modelosLocacaoMove.Modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme objetoDaClasseFilme = new Filme("O poderoso chefão",1979);
+        var filmeDoPaulo = new Filme("DogVille", 2003);
+        Filme outroObjtFilme = new Filme("Outro filme",2025 );
+        Serie objetoSerieLost = new Serie("Lost",2003 );
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+
+        lista.add(filmeDoPaulo);
+        lista.add(objetoDaClasseFilme);
+        lista.add(outroObjtFilme);
+        lista.add(objetoSerieLost);
+
+        for (Titulo item: lista){
+            System.out.println(item);
+        }
+    }
+}
