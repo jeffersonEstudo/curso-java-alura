8aeb1d0d1fe51dd8f261bf4c3feb9046998a349c
Jefferson-Code16 <jefferson.amorim@solutis.com.br>
2025-04-09 21:23:58 -0300

    DESAFIOfinal

--- src/br/com/alura/DESAFIOFinal/AplicationPrincipal.java
+++ src/br/com/alura/DESAFIOFinal/AplicationPrincipal.java
@@ -6,30 +6,37 @@ import java.util.Scanner;
 
 public class AplicationPrincipal {
     public static void main(String[] args) {
+        Scanner optRep = new Scanner(System.in);
+        int repOpt = 1;
+
+        while (repOpt == 1) {
+            Scanner entrada = new Scanner(System.in);
+            List<Saldo> listSaldo = new ArrayList<>();
+
+            System.out.println("Digite seu saldo: ");
+            double entradaSaldo = entrada.nextDouble();
+
+            System.out.println("Digite a descrição da compra:");
+            String entradaProduto = entrada.next();
+
+            System.out.println("Digite o valor da compra:");
+            double entradaValor = entrada.nextDouble();
+
+            Saldo refObjSaldo = new Saldo(entradaSaldo, entradaProduto, entradaValor);
+            listSaldo.add(refObjSaldo);
 
 
+            for (Saldo teste : listSaldo) {
+                System.out.println(teste.toString());
 
-        Scanner entrada = new Scanner(System.in);
-        List<Saldo> listSaldo = new ArrayList<>();
+            }
 
-        System.out.println("Digite seu saldo: ");
-        double entradaSaldo = entrada.nextDouble();
-
-        System.out.println("Digite a descrição da compra:");
-        String entradaProduto = entrada.next();
-
-        System.out.println("Digite o valor da compra:");
-        double entradaValor = entrada.nextDouble();
-
-       Saldo refObjSaldo =new Saldo(entradaSaldo,entradaProduto,entradaValor);
-        listSaldo.add(refObjSaldo);
+            repOpt = 0;
+            System.out.println("Digite 0 para sair e 1 para continuar:");
+            repOpt = optRep.nextInt() ;
 
 
-            for (Saldo teste: listSaldo) {
-            System.out.println(teste.toString());
         }
-
-
 
 
     }
