2dc061407b4c35d9d95416f793523bbb1e63f19c
Jefferson-Code16 <jefferson.amorim@solutis.com.br>
2025-04-12 14:10:47 -0300

    CURSO4ConsumindoAPI

---
+++ src/LocaçãoMove/PrincipalComBusca.java
@@ -0,0 +1,27 @@
+package LocaçãoMove;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner entradaLeitura = new Scanner(System.in);
+        System.out.println("Digite um filme:");
+        String busca = entradaLeitura.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=f301d24";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
