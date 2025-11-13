a2b683199bd54222b12c89bcbc0e1c4164faca24
jeffersonEstudo <jefferson11oliveira@gmail.com>
2025-11-13 16:22:55 -0300

    PesquisandoUserNoGIT

---
+++ src/br/com/alura/PraticandoCurso4Modulo3/PraticandoNovamente/ErroIlegalArgumento.java
@@ -0,0 +1,7 @@
+package br.com.alura.PraticandoCurso4Modulo3.PraticandoNovamente;
+
+public class ErroIlegalArgumento extends RuntimeException {
+  public ErroIlegalArgumento(String message) {
+    super(message);
+  }
+}
